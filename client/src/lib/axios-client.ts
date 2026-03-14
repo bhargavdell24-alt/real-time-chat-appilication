c81f9c99d4a23c135f0d3ed53de9cad68075ffe3
Bhargav Jani <bhargavdell24@gmail.com>
2026-03-14 20:41:49 +0530

    temporary hardcoded backend url

--- client/src/lib/axios-client.ts
+++ client/src/lib/axios-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: "https://real-time-chat-appilication-backend.onrender.com",
   withCredentials: true,
 });
